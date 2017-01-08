67480d180362fa923e6b92585e545e5769ac7905
amitu <amitur91@gmail.com>
2017-01-08 16:56:57 +0200

    start

--- src/main/java/bgu/spl171/net/impl/ConnectionsImpl.java
+++ src/main/java/bgu/spl171/net/impl/ConnectionsImpl.java
@@ -1,32 +1,40 @@
 package bgu.spl171.net.impl;
 
 import bgu.spl171.net.srv.ConnectionHandler;
-
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Map;
+import java.util.Set;
+import java.util.concurrent.ConcurrentHashMap;
 
 /**
  * Created by amitu on 07/01/2017.
  */
 public class ConnectionsImpl<T> implements bgu.spl171.net.api.Connections<T>{
 
-    ArrayList<ConnectionHandler<T>> MyConnections = new ArrayList<>();
+    Integer nextId=0;
+    ConcurrentHashMap<Integer ,ConnectionHandler<T>> MyConnections = new ConcurrentHashMap<Integer ,ConnectionHandler<T>>();
+
+    public void addConnection(ConnectionHandler<T> CH){
+        MyConnections.put(nextId++,CH);
+    }
 
     @Override
     public boolean send(int connectionId, Object msg) {
-        if (MyConnections.get(connectionId)!=null) {
+        if (MyConnections.containsKey(connectionId)) {
             MyConnections.get(connectionId).send((T) msg);
             return true;
         }
-
         return false;
     }
 
     @Override
     public void broadcast(Object msg) {
-    for (ConnectionHandler<T> Con :MyConnections){
+        Collection<ConnectionHandler<T>> all = MyConnections.values();
+            for (ConnectionHandler<T> Con :all){
         Con.send((T)msg);
-    }
+        }
     }
 
     @Override
--- src/main/java/bgu/spl171/net/srv/NonBlockingConnectionHandler.java
+++ src/main/java/bgu/spl171/net/srv/NonBlockingConnectionHandler.java
@@ -1,5 +1,6 @@
 package bgu.spl171.net.srv;
 
+import bgu.spl171.net.api.BidiMessagingProtocol;
 import bgu.spl171.net.api.MessageEncoderDecoder;
 import bgu.spl171.net.api.MessagingProtocol;
 import com.sun.xml.internal.bind.v2.TODO;
@@ -18,22 +19,28 @@ public class NonBlockingConnectionHandler<T> implements ConnectionHandler<T> {
     private static final int BUFFER_ALLOCATION_SIZE = 1 << 13; //8k
     private static final ConcurrentLinkedQueue<ByteBuffer> BUFFER_POOL = new ConcurrentLinkedQueue<>();
 
-    private final MessagingProtocol<T> protocol;
+    private final BidiMessagingProtocol<T> protocol;
     private final MessageEncoderDecoder<T> encdec;
     private final Queue<ByteBuffer> writeQueue = new LinkedList<>();
     private final SocketChannel chan;
     private final Reactor reactor;
-    //id?
+
+    public int getID() {
+        return ID;
+    }
+
+    private final int ID;
 
     public NonBlockingConnectionHandler(
             MessageEncoderDecoder<T> reader,
-            MessagingProtocol<T> protocol,
+            BidiMessagingProtocol<T> protocol,
             SocketChannel chan,
-            Reactor reactor) {
+            Reactor reactor, int ID) {
         this.chan = chan;
         this.encdec = reader;
         this.protocol = protocol;
         this.reactor = reactor;
+        this.ID = ID;
     }
 
     public Runnable continueRead() {
